02dea4e65a065b98beff073de1c33e05447e4a35
Sam <pmxsh6@nottingham.ac.uk>
2019-03-12 17:42:13 +0000

    added level 2
    made level 2

---
+++ LabryinthGame Level 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	//public float speed;
+	public Text countText;
+	public Text winText;
+
+	//private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	//when player object touches trigger collider (other)
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject); //destroys object that causes trigger
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+		if (other.gameObject.CompareTag ("Cylinder")) {
+			winText.text = "GAME OVER";
+			GameObject.Destroy (gameObject);
+			Time.timeScale = 0;
+		}
+	}
+
+
+	void setCountText(){
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 10) {
+			winText.text = "Great Score!!";
+		}
+
+	}
+}
