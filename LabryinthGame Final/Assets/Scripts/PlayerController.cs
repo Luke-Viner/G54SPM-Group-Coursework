d1b8420738187c033779a231a30354b93b10ed81
Sam <pmxsh6@nottingham.ac.uk>
2019-03-14 21:51:22 +0000

    Menu, Level 1, Level 2
    Working version of Level 1, Level 2 and the main menu

---
+++ LabryinthGame Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	//public float speed;
+	public Text countText;
+	public Text winText;
+    public Vector3 spherePos;// = new Vector3(22.87f, -0.35f, -21.9f);
+    public Vector3 offset = new Vector3(0.0f, 3.0f, 0.0f);
+    private Rigidbody rb;
+	private int count;
+    public GameObject[] gameObjects;
+
+    // Use this for initialization
+    void Start () {
+        gameObjects = GameObject.FindGameObjectsWithTag("Pick Up");
+        rb = GetComponent<Rigidbody>();
+        spherePos = rb.position;
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	//when player object touches trigger collider (other)
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject); //destroys object that causes trigger
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+		if (other.gameObject.CompareTag ("Cylinder")) {
+            rb.transform.position = spherePos + offset;
+            activateObjects();
+            count = 0;
+            setCountText();
+
+		}
+        if (other.gameObject.CompareTag ("Win Zone")) {
+            GameObject.Destroy (gameObject);
+            countText.text = "YOU WON";
+            Time.timeScale = 0;
+        }
+	}
+
+	void setCountText(){
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 10) {
+			winText.text = "Great Score!!";
+		}
+
+	}
+		
+	void update(){
+	}
+
+    public void activateObjects()
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            if (!go.activeInHierarchy)
+            {
+                go.SetActive(true);
+            }
+        }
+    }
+}
