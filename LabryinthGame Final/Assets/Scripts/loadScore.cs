9be836a0c313d4fae75a45c49c41178b77fe6c5a
Luke-Viner <luke_viner@hotmail.co.uk>
2019-03-17 19:41:00 +0000

    Added score screen
    Score has been added to game

---
+++ LabryinthGame Final/Assets/Scripts/loadScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadScore : MonoBehaviour
+{
+    public Text score;
+
+    public void updateScore()
+    {
+        
+    }
+}
