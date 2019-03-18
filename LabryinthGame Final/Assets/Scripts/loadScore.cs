c3f8f49b6bd35b59744545ca41a7ec5719f46c6a
Luke-Viner <luke_viner@hotmail.co.uk>
2019-03-18 18:42:14 +0000

    Fixed scoring system and finished game code
    as above

---
+++ LabryinthGame Final/Assets/Scripts/loadScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadScore : MonoBehaviour
+{
+    public Text scoreText;
+
+    void Start()
+    {
+        Debug.Log("Score: " + PlayerPrefs.GetInt("score"));
+        scoreText.text = PlayerPrefs.GetInt("score").ToString();
+        PlayerPrefs.DeleteAll();
+    }
+}
